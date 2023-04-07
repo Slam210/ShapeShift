39070f9e96ed8f5467f3ace59ddb8711d9b4b923
Steven Lam <slam@mail.csuchico.edu>
2023-04-07 14:32:17 -0700

    Fixed Settings Page and cleaned Project files

--- shapeshift/lib/main.dart
+++ shapeshift/lib/main.dart
@@ -363,7 +363,7 @@ class ResetPasswordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final TextEditingController emailController = TextEditingController();
-    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
     String errorMessage = '';
 
     return Scaffold(
@@ -374,7 +374,7 @@ class ResetPasswordPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Form(
-            key: _formKey,
+            key: formKey,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -401,7 +401,7 @@ class ResetPasswordPage extends StatelessWidget {
                 const SizedBox(height: 16.0),
                 ElevatedButton(
                   onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
+                    if (formKey.currentState!.validate()) {
                       String email = emailController.text;
                       try {
                         if (kDebugMode) {
@@ -510,10 +510,10 @@ class _HomePageState extends State<HomePage> {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const RoutinesPage()),
+                  MaterialPageRoute(builder: (context) => const GroupsPage()),
                 );
               },
-              child: const Text('Go to Settings Page'),
+              child: const Text('Go to Groups Page'),
             ),
             ElevatedButton(
               onPressed: () {
@@ -559,97 +559,99 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Settings Page'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'User Profile',
-              style: Theme.of(context).textTheme.headline6,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'User Profile',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
             ),
-          ),
-          const Divider(),
-          ListTile(
-            title: const Text('Email'),
-            subtitle: Text(FirebaseAuth.instance.currentUser!.email ?? ''),
-          ),
-          ListTile(
-            title: const Text('Username'),
-            subtitle: const Text('John Doe'), // Replace with user's username
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Change Password',
-              style: Theme.of(context).textTheme.headline6,
+            const Divider(),
+            ListTile(
+              title: const Text('Email'),
+              subtitle: Text(FirebaseAuth.instance.currentUser!.email ?? ''),
             ),
-          ),
-          const Divider(),
-          ListTile(
-            title: const Text('Current Password'),
-            subtitle: const TextField(),
-          ),
-          ListTile(
-            title: const Text('New Password'),
-            subtitle: const TextField(),
-          ),
-          ListTile(
-            title: const Text('Confirm New Password'),
-            subtitle: const TextField(),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Data Management',
-              style: Theme.of(context).textTheme.headline6,
+            const ListTile(
+              title: Text('Username'),
+              subtitle: Text('John Doe'), // Replace with user's username
             ),
-          ),
-          const Divider(),
-          ListTile(
-            title: const Text('Clear Data'),
-            subtitle: const Text('Delete all workout routines'),
-            trailing: const Icon(Icons.delete),
-            onTap: () {
-              // I should put something here
-            },
-          ),
-          ListTile(
-            title: const Text('Delete All Routines and Quit All Groups'),
-            subtitle: const Text('Delete all routines and leave all groups'),
-            trailing: const Icon(Icons.delete_forever),
-            onTap: () {
-              // we should implement delete all routines and quit all groups functionality
-            },
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const SignInPage()),
-              );
-            },
-            child: const Text('Log Out'),
-          ),
-        ],
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Change Password',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+            const Divider(),
+            const ListTile(
+              title: Text('Current Password'),
+              subtitle: TextField(),
+            ),
+            const ListTile(
+              title: Text('New Password'),
+              subtitle: TextField(),
+            ),
+            const ListTile(
+              title: Text('Confirm New Password'),
+              subtitle: TextField(),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Data Management',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+            const Divider(),
+            ListTile(
+              title: const Text('Clear Data'),
+              subtitle: const Text('Delete all workout routines'),
+              trailing: const Icon(Icons.delete),
+              onTap: () {
+                // I should put something here
+              },
+            ),
+            ListTile(
+              title: const Text('Delete All Routines and Quit All Groups'),
+              subtitle: const Text('Delete all routines and leave all groups'),
+              trailing: const Icon(Icons.delete_forever),
+              onTap: () {
+                // we should implement delete all routines and quit all groups functionality
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignInPage()),
+                );
+              },
+              child: const Text('Log Out'),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-class RoutinesPage extends StatelessWidget {
-  const RoutinesPage({Key? key}) : super(key: key);
+class GroupsPage extends StatelessWidget {
+  const GroupsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Routines Page (under construction)'),
+        title: const Text('Groups Page (under construction)'),
       ),
       body: Center(
         child: Column(
