8e6628b9da595c3c97c6be09bcc85184312814b5
Alexander Haga <arhaga@csuchico.edu>
2023-04-07 21:14:24 +0000

    Settings Pt1

--- shapeshift/lib/main.dart
+++ shapeshift/lib/main.dart
@@ -557,26 +557,86 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Settings Page (under construction)'),
+        title: const Text('Settings Page'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('This will be cool in a few weeks.'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SignInPage()),
-                );
-              },
-              child: const Text('Log Out'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'User Profile',
+              style: Theme.of(context).textTheme.headline6,
             ),
-          ],
-        ),
+          ),
+          const Divider(),
+          ListTile(
+            title: const Text('Email'),
+            subtitle: Text(FirebaseAuth.instance.currentUser!.email ?? ''),
+          ),
+          ListTile(
+            title: const Text('Username'),
+            subtitle: const Text('John Doe'), // Replace with user's username
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Change Password',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          const Divider(),
+          ListTile(
+            title: const Text('Current Password'),
+            subtitle: const TextField(),
+          ),
+          ListTile(
+            title: const Text('New Password'),
+            subtitle: const TextField(),
+          ),
+          ListTile(
+            title: const Text('Confirm New Password'),
+            subtitle: const TextField(),
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Data Management',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          const Divider(),
+          ListTile(
+            title: const Text('Clear Data'),
+            subtitle: const Text('Delete all workout routines'),
+            trailing: const Icon(Icons.delete),
+            onTap: () {
+              // I should put something here
+            },
+          ),
+          ListTile(
+            title: const Text('Delete All Routines and Quit All Groups'),
+            subtitle: const Text('Delete all routines and leave all groups'),
+            trailing: const Icon(Icons.delete_forever),
+            onTap: () {
+              // we should implement delete all routines and quit all groups functionality
+            },
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const SignInPage()),
+              );
+            },
+            child: const Text('Log Out'),
+          ),
+        ],
       ),
     );
   }
